d94115d70137abbdb88ac3d94e7a00e068c53cc2
jonesbr <brandan.jones@uc.edu>
2020-11-22 17:51:38 -0500

    Changes to deploy to Azure and use Azure Event Hub.  I recorded the changes in this video: https://youtu.be/Y2yHJc0a5fo

--- src/main/java/com/myplantdiary/postprocessor/photoprocessor/PhotoProcessor.java
+++ src/main/java/com/myplantdiary/postprocessor/photoprocessor/PhotoProcessor.java
@@ -20,27 +20,12 @@ public class PhotoProcessor {
     @Autowired
     KafkaTemplate<String, String> kafkaTemplate;
 
-    @KafkaListener(topics="photoIn", groupId="myplantdiary")
+    @KafkaListener(topics="photoin", groupId="photoprocessor")
     public void processPhoto(String path) {
         System.out.println("Path is: " + path);
-        File file = new File(path);
-        Path photoPath = Paths.get(path).getParent();
-        Path fileName = Paths.get(path).getFileName();
-        String thumbnailPath = photoPath + File.separator + "thumbnail" + File.separator + fileName;
-        File thumbnailFile = new File(thumbnailPath);
-
-        String watermarkPath = photoPath + File.separator + "watermark.png";
-        File watermarkFile = new File(watermarkPath);
-
-        try {
-
-            BufferedImage watermark = ImageIO.read(watermarkFile);
-            Thumbnails.of(file).scale(1).watermark(Positions.BOTTOM_RIGHT, watermark, 0.9F).toFile(file);
-            Thumbnails.of(file).size(100, 100).toFile(thumbnailFile);
-
+        if (path.contains("Photo")) {
             kafkaTemplate.send("photoOut", path);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
             kafkaTemplate.send("photoException", path);
         }
 
---
+++ src/main/java/com/myplantdiary/postprocessor/photoprocessor/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.myplantdiary.postprocessor.photoprocessor;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PhotoprocessorApplication.class);
+    }
+
+}
